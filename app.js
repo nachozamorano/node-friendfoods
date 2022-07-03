40a606948786d2847bec1e7ff8416658a73c1784
Ignacio Zamorano <nachozamorano94@gmail.com>
2022-07-03 19:10:40 -0400

    CHG: Se corrige error de cabecera
    Signed-off-by: Ignacio Zamorano <nachozamorano94@gmail.com>

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+var cors = require('cors');
 const env = require('./env');
 
 const routes = require('./routes/index');
 
 const app = express();
 app
+    .use(cors())
     .use(morgan('dev'))
     .use(bodyParser.urlencoded({ extended: false }))
     .use(bodyParser.json())
