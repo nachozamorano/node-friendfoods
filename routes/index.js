17a51631d371f04821f4f2f7f897fc074b38824a
Ignacio Zamorano <nachozamorano94@gmail.com>
2022-07-03 16:37:28 -0400

    ADD: Se agrega consulta para solo tipo de plato
    Signed-off-by: Ignacio Zamorano <nachozamorano94@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const router = express.Router();
 
 router
-.use('/restaurant', require('./restaurant'));
+.use('/restaurant', require('./restaurant'))
+.use('/tipoplato', require('./tipoplato'));
 
 module.exports = router;
--- routes/tipoplato.js
+++ routes/tipoplato.js
@@ -4,21 +4,21 @@ const database = require ('../database');
 const router = express.Router();
 
 router
-    //Consultando restaurant platos con su tipo
+    //Consultando restaurant con su tipo
     .post('/', async (req, res) => {
         const { id } = req.body;
         try {
             const data = await database.query(
-                'SELECT pl.IdPlato, pl.NombrePlato, pl.Precio, pl.detalle, pl.URLImagen, tp.IdTipoPlato, tp.NombreTipo FROM restaurante res INNER JOIN plato pl ON pl.FK_IdTipoPlato = res.IdRestaurante INNER JOIN tipo_de_plato tp ON tp.IdTipoPlato = pl.FK_IdTipoPlato WHERE res.IdRestaurante = ' + id
+                'SELECT tp.IdTipoPlato, tp.NombreTipo FROM restaurante res INNER JOIN plato pl ON pl.FK_IdTipoPlato = res.IdRestaurante INNER JOIN tipo_de_plato tp ON tp.IdTipoPlato = pl.FK_IdTipoPlato WHERE res.IdRestaurante = ' + id
             )
             res.contentType('json');
             res.status(200);
             res.json(data);
         } catch (e) {
             console.log(e);
-            console.error('Error consultando restaurant');
+            console.error('Error consultando tipo');
             res.status(500);    
-            res.end('Error consultando restaurant, Estas seguro que existe?');
+            res.end('Error consultando tipo, Estas seguro que existe?');
         }
 
     });
