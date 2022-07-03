ab9c9a90eb5237871e6c39081638ab46fa2783a6
Ignacio Zamorano <nachozamorano94@gmail.com>
2022-07-03 02:05:46 -0400

    change port
    Signed-off-by: Ignacio Zamorano <nachozamorano94@gmail.com>

--- env.js
+++ env.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const env = process.env;
 module.exports = {
-    port: env.PORT || 8080,
+    port: env.PORT || 5000,
     CLEARDB_DATABASE_URL: process.env.CLEARDB_DATABASE_URL,
 }
