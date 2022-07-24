93a3cc4b502088ad716b32ac21d55a070c4c7f3a
Ignacio Zamorano <nachozamorano94@gmail.com>
2022-07-24 13:03:02 -0400

    ADD: Se agrega parametro si esta bloqueado el restaurant
    Signed-off-by: Ignacio Zamorano <nachozamorano94@gmail.com>

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -9,7 +9,7 @@ router
         const { id } = req.body;
         try {
             const data = await database.query(
-                'SELECT pl.IdPlato, pl.NombrePlato, pl.Precio, pl.detalle, pl.URLImagen, tp.IdTipoPlato, tp.NombreTipo FROM restaurante res INNER JOIN plato pl ON pl.FK_IdRestaurante = res.IdRestaurante INNER JOIN tipo_de_plato tp ON tp.IdTipoPlato = pl.FK_IdTipoPlato WHERE res.IdRestaurante = ' + id
+                'SELECT pl.IdPlato, pl.NombrePlato, pl.Precio, pl.detalle, pl.URLImagen, tp.IdTipoPlato, tp.NombreTipo, res.FK_idStatus FROM restaurante res INNER JOIN plato pl ON pl.FK_IdRestaurante = res.IdRestaurante INNER JOIN tipo_de_plato tp ON tp.IdTipoPlato = pl.FK_IdTipoPlato WHERE res.IdRestaurante = ' + id
             )
             res.contentType('json');
             res.status(200);
