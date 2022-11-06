0b3045c4be395a219704de4dfed4dec838271960
Ignacio Zamorano <nachozamorano94@gmail.com>
2022-11-06 20:41:35 -0300

    ADD: Se agrega consulta estado orden
    Signed-off-by: Ignacio Zamorano <nachozamorano94@gmail.com>

---
+++ routes/estadoorden.js
@@ -0,0 +1,25 @@
+const { json } = require('body-parser');
+const express = require ('express')
+const database = require ('../database');
+const router = express.Router();
+
+router
+    //Consultando restaurant platos con su tipo
+    .post('/', async (req, res) => {
+        const { id, num } = req.body;
+        try {
+            var data = await database.query(
+                'SELECT ord.FK_IdEstado FROM orden ord INNER JOIN mesa me ON ord.FK_IdMesa = me.IdMesa WHERE me.FK_IDRestaurante ='+id+' AND me.numero ='+num
+            )
+            res.contentType('json');
+            res.status(200);
+            res.json(data);
+        } catch (e) {
+            console.log(e);
+            console.error('Error consultando estado de la orden');
+            res.status(500);    
+            res.end('Error consultando estado de la orden, Estas seguro que existe?');
+        }
+
+    });
+module.exports = router;
