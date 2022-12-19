13421f3bee47661bc7761477ab3556524773bb0c
Ignacio Zamorano <nachozamorano94@gmail.com>
2022-12-19 02:40:49 -0300

    ADD: Se agrega insertar orden
    Signed-off-by: Ignacio Zamorano <nachozamorano94@gmail.com>

--- routes/orden.js
+++ routes/orden.js
@@ -21,5 +21,32 @@ router
             res.end('Error consultando estado de la orden, Estas seguro que existe?');
         }
 
+    })
+    .post('/insertar', async (req, res) => {
+        const { id, num, total, rut, nombre, subtotal} = req.body;
+        try {
+            
+            var boleta = await database.query(
+                'INSERT INTO boleta (fecha,total) VALUES (NOW() - INTERVAL 3 HOUR, '+total+')'
+            )
+
+            var idMesa = await database.query(
+                'SELECT IdMesa FROM mesa WHERE FK_IdRestaurante = '+ id + ' AND numero = '+ num
+            )
+            var data = await database.query(
+                'INSERT INTO orden (FK_IdMesero, NombreCliente, fecha, FK_IdEstado, Subtotal, FK_IdBoleta, FK_IdMesa) VALUES ('+rut+', \''+nombre+'\', NOW() - INTERVAL 3 HOUR, 1, '+subtotal+', '+boleta.insertId+', '+idMesa[0].IdMesa+')'
+            )
+            
+            res.contentType('json');
+            res.status(200);
+            res.json(data);
+        } catch (e) {
+            console.log(e);
+            console.error('Error al insertar la orden');
+            res.status(500);    
+            res.end('Error al insertar la orden');
+        }
+
     });
+
 module.exports = router;
